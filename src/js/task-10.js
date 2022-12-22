a6797e86ca274c8ab5a4a78d2726480a3a985d18
Mariia Khapun <mariakhapun@gmail.com>
2022-12-22 15:58:39 +0000

    task 10 begin

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,44 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const divBoxes = document.querySelector(`#boxes`);
+console.log(divBoxes);
+
+const inputEl = document.querySelector(`input`);
+console.log(inputEl);
+
+const createBtn = document.querySelector(`[data-create]`);
+console.log(createBtn);
+
+const destroyBtn = document.querySelector(`[data-destroy]`);
+console.log(destroyBtn);
+
+
+
+
+function createBoxes() {
+  const amount = inputEl.value;
+
+  for (let i = 1; i <= amount; i += 1) {
+    const divEl = document.createElement(`div`);
+
+    divEl.style.width = 30 + "px";
+    divEl.style.height = 30 + "px";
+    divEl.style.backgroundColor = getRandomHexColor();
+
+    divBoxes.append(divEl);
+  }
+}
+
+createBtn.addEventListener(`click`, createBoxes);
+
+destroyBtn.addEventListener(`click`, () => {
+  divBoxes.innerHTML = "";
+  inputEl.value = "";
+});
+
+
+
+
+
